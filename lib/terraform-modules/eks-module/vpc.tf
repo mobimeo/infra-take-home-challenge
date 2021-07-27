49d8ff48954ef71d485673bfbec9ad0531eb5289
pankaj motwani <p.motwani@searchmetrics.com>
2021-07-27 15:52:14 +0200

    added EKS module

---
+++ lib/terraform-modules/eks-module/vpc.tf
@@ -0,0 +1,62 @@
+variable "region" {
+  default     = "eu-west-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region     = "eu-west-1"
+# Created aws profile test-kube locally , exported test-kube awsd profile and referring here to pass the aws provider creds #
+# We can also pass the credentials with Environment variables, used both the method #
+# profile = "test-kube"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.66.0"
+
+  name                 = "eks-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
