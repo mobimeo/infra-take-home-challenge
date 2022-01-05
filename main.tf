18ed16c9bd8f8dbe4d2855fe528b878a0b4ead63
varilci <batuhanvarilci@gmail.com>
2022-01-05 17:24:31 +0300

    terraform-modules/app: init
    - Modified the `app` module. Now `terraform apply` deploys a simple k8s cluster with `hello-kubernetes`
    image. An service ingress added to simple access to the greeting page, which runs from port 8080. Get
    the service ingress address from `kubectl` to display it via web browser.
    
    - Added `.gitignore` for very basic "secret" file management. `terraform init` and `terraform apply` should
    recreate necessary files and the kubeconfig. Other credentials are assumed to be configured via `aws cli`.
    
    - Modified the provider versions via `providers.tf`.

--- main.tf
+++ main.tf
@@ -3,16 +3,56 @@ module "app" {
   namespace         = "app"
 }
 
-module "consent_service_postgres" {
-  source            = "./lib/terraform-modules/rds-postgres"
-  team              = "Foo"
-  identifier        = "App"
-  instance_class    = "db.t3.small"
-  allocated_storage = 20
-  username          = "foo"
-  password          = "bar"
-  subnet_ids        = module.cluster.private_subnets_ids
-  vpc_id            = module.cluster.vpc_id
-  cidr_blocks       = module.cluster.private_subnets_cidr_blocks
-  dns_zone          = module.cluster.internal_r53_zone_id
+locals {
+  cluster_name = "mobimeo-infra-the-kubernetes"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.11.0"  
+
+  name                 = "mobimeo-the-vpc"
+  cidr                 = "172.31.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.31.1.0/24", "172.31.2.0/24", "172.31.3.0/24"]
+  public_subnets       = ["172.31.4.0/24", "172.31.5.0/24", "172.31.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
+
+  cluster_name    = "${local.cluster_name}"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 5
+      min_capacity     = 1
+
+      instance_type = "t2.micro"
+    }
+  }
+  
+  write_kubeconfig   = true
+  kubeconfig_output_path = "./"
 }
